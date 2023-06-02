2cdf77651fef0d8d1e4904193bb4874f7160f081
wildanrizkii <wildanrizki9560@gmail.com>
2023-06-02 18:31:22 +0700

    menambah session pada login

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql';
 import express from 'express';
-import path, { resolve } from 'path';
+import path from 'path';
 import crypto from 'crypto';
 import session from 'express-session';
 
@@ -39,10 +39,73 @@ const dbConnect = () => {
     });
 };
 
+const getCheckAdmin = (conn, username, hashed_pass) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${hashed_pass}'`, (err, result) => {
+            if(err){
+                reject(err);
+            } else{
+                resolve(result);
+            }
+        });
+    });
+};
+
+const getCheckMember = (conn, username, hashed_pass) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM member WHERE username = '${username}' AND password = '${hashed_pass}'`, (err, result) => {
+            if(err){
+                reject(err);
+            } else{
+                resolve(result);
+            }
+        });
+    });
+};
+
 app.get('/', async (req, res) => {
-    // const conn = await dbConnect();
-    res.render('login')
-})
+    let data = "";
+    res.render('login', {data})
+});
+
+app.get('/login', async (req, res) => {
+    let data = "";
+    res.render('login', {data})
+});
+
+app.get('/daftar', async (req, res) => {
+    const conn = await dbConnect();
+    res.render('daftar')
+});
+
+//POST METHOD
+
+app.post('/login', async (req, res) => {
+    const conn = await dbConnect();
+    const { username, password } = req.body;
+    const hashed_pass = crypto.createHash('sha256').update(password).digest('base64');
+    let data = "";
+    // console.log(hashed_pass);
+    if (username.length > 0 && password.length > 0){
+        const dataAdmin = await getCheckAdmin(conn, username, hashed_pass);
+        const dataMember = await getCheckMember(conn, username, hashed_pass);
+        
+        if (dataAdmin.length > 0){
+            req.session.data = dataAdmin[0].nama;
+            console.log(req.session.data);
+            res.redirect('/daftar');
+        } else if (dataMember.length > 0){
+            req.session.data = dataMember[0].nama;
+            console.log(req.session.data);
+            res.redirect('/daftar');
+        } else{
+            data = "Data tidak ditemukan!";
+            res.render('login', {data});
+        }
+        
+    }
+    conn.release();
+});
 
 app.listen(PORT, () => {
     console.log("Ready!")
