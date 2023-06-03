cbefdc56f19eaa0e69cf9260fd58ac0366317de5
Wildan Rizki Nurfauzi <6181901064@student.unpar.ac.id>
2023-06-03 20:45:36 +0700

    sudah bisa update data member

--- index.js
+++ index.js
@@ -87,17 +87,29 @@ const getDataMember = conn => {
     });
 };
 
-// const getDataEdit = (conn, data) => {
-//     return new Promise((resolve, reject) => {
-//         conn.query(`SELECT * FROM member WHERE username = ${data}`, (err, result) => {
-//             if(err){
-//                 reject(err);
-//             } else{
-//                 resolve(result);
-//             }
-//         });
-//     });
-// };
+const getDataEdit = (conn, username) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM member WHERE username = '${username}'`, (err, result) => {
+            if(err){
+                reject(err);
+            } else{
+                resolve(result);
+            }
+        });
+    });
+};
+
+const updateDataMember = (conn, data, nama, username, no_hp, alamat) => {
+    return new Promise((resolve, reject) => {
+        conn.query(`UPDATE member SET nama = '${nama}', username = '${username}', no_hp = '${no_hp}', alamat = '${alamat}' WHERE username = '${data}'`, (err, result) => {
+            if(err){
+                reject(err);
+            } else{
+                resolve(result);
+            }
+        });
+    });
+};
 
 const isAuthMember = (req, res, next) => {
     if (req.session.isAuthMember){
@@ -136,30 +148,30 @@ app.get('/daftar', async (req, res) => {
 });
 
 app.get('/homeAdmin', isAuthAdmin, async (req, res) => {
-    let nama = req.session.data;
+    let dataSession = req.session.data;
     console.log("Auth Admin : " + req.session.isAuthAdmin)
-    res.render('homeAdmin', {nama})
+    res.render('homeAdmin', {dataSession})
 });
 
 app.get('/homeMember', isAuthMember, async (req, res) => {
-    let nama = req.session.data
+    let dataSession = req.session.data
     console.log("Auth Member : " + req.session.isAuthMember)
-    res.render('homeMember', {nama})
+    res.render('homeMember', {dataSession})
 });
 
 app.get('/dataMember', isAuthAdmin, async (req, res) => {
     const conn = await dbConnect();
-    let nama = req.session.data;
-    let data = await getDataMember(conn);
-    res.render('dataMember', {nama, data})
+    let dataSession = req.session.data;
+    let dataMember = await getDataMember(conn);
+    res.render('dataMember', {dataSession, dataMember})
 });
 
-app.get('/edit/:username', async (req, res) => {
+app.get('/edit/:data', async (req, res) => {
     const conn = await dbConnect();
-    const {username} = req.params
-    conn.query(`SELECT * FROM member WHERE username = ${username}`, (err, res) => {
-        res.render('dataMember')
-    });
+    let dataSession = req.session.data
+    let dataMember = await getDataMember(conn);
+    res.render('dataMember', {dataSession, dataMember})
+
 });
 
 app.get('/logout', async (req, res) => {
@@ -216,8 +228,28 @@ app.post('/daftar', async (req, res) => {
     if (nama != undefined && username != undefined && status == true && nomorhp != undefined && alamat != undefined){
         const insert = await postDaftar(conn, nama, username, hashed_pass, nomorhp, alamat);
     }
-    res.render('login', {data})
+    res.render('login', {dataSession})
     conn.release();
+});
+
+app.post('/edit/:data', async (req, res) => {
+    const conn = await dbConnect();
+    const {data} = req.params
+    const {username, nama, nomorhp, alamat} = req.body
+    // console.log(data)
+    // console.log(nama)
+    // console.log(username)
+    // console.log(nomorhp)
+    // console.log(alamat)
+    // console.log(req.session.data)
+
+    let dataSession = req.session.data
+    let dataMember = await getDataMember(conn);
+    const dataEdit = await getDataEdit(conn, data)
+    const updateData = await updateDataMember(conn, data, nama, username, nomorhp, alamat)
+
+    conn.release();
+    res.redirect('/dataMember')
 });
 
 app.listen(PORT, () => {
--- public/js/dataMember.js
+++ public/js/dataMember.js
@@ -1,25 +1,3 @@
-const modalBtn = document.getElementById("modalBtn");
-const modalOverlay = document.getElementById("modalOverlay");
-const modal = document.getElementById("myModal");
-const modalClose = document.getElementById("modalClose");
-
-console.log(modalBtn);
-
 (function() {
-    modalBtn.addEventListener('click', () => {
-        modalOverlay.style.display = "block";
-        modal.style.display = "block";
-    })
 
-    modalClose.addEventListener('click', () => {
-        modalOverlay.style.display = "none";
-        modal.style.display = "none";
-    })
-
-    window.addEventListener('click', () => {
-        if (event.target == modalOverlay) {
-            modalOverlay.style.display = "none";
-            modal.style.display = "none";
-        }
-    })
 })();
