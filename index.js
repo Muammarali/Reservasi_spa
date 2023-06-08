530911206fa9e7a8723d125ea19dd0ed5574baf7
cornel <rafirahman30@icloud.com>
2023-06-08 16:30:04 +0700

    member sudah tinggal add db dan gambar

--- index.js
+++ index.js
@@ -1,283 +1,345 @@
-import mysql from 'mysql';
-import express from 'express';
-import path from 'path';
-import crypto from 'crypto';
-import session from 'express-session';
+import mysql from "mysql";
+import express from "express";
+import path from "path";
+import crypto from "crypto";
+import session from "express-session";
 
 const pool = mysql.createPool({
-    user: 'root',
-    password: '',
-    database: 'balihalus_db',
-    host: 'localhost'
+  user: "root",
+  password: "",
+  database: "balihalus_db",
+  host: "localhost",
 });
 
 const PORT = 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const staticPath = path.resolve('public');
+const staticPath = path.resolve("public");
 app.use(express.static(staticPath));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: 'mysecretkey',
+app.use(
+  session({
+    secret: "mysecretkey",
     resave: false,
     saveUninitialized: false,
-    cookie: {secure: false}
-}));
+    cookie: { secure: false },
+  })
+);
 
 const dbConnect = () => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(conn);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, conn) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(conn);
+      }
     });
+  });
 };
 
 const getCheckAdmin = (conn, username, hashed_pass) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`SELECT * FROM admin WHERE username = '${username}' AND password = '${hashed_pass}'`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `SELECT * FROM admin WHERE username = '${username}' AND password = '${hashed_pass}'`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
 const getCheckMember = (conn, username, hashed_pass) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`SELECT * FROM member WHERE username = '${username}' AND password = '${hashed_pass}'`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `SELECT * FROM member WHERE username = '${username}' AND password = '${hashed_pass}'`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
 const postDaftar = (conn, nama, username, hashed_pass, no_hp, alamat) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`INSERT INTO member (nama, username, password, no_hp, alamat) VALUES ('${nama}', '${username}', '${hashed_pass}', '${no_hp}', '${alamat}')`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `INSERT INTO member (nama, username, password, no_hp, alamat) VALUES ('${nama}', '${username}', '${hashed_pass}', '${no_hp}', '${alamat}')`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
-const getDataMember = conn => {
-    return new Promise((resolve, reject) => {
-        conn.query(`SELECT * FROM member`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
+const getDataMember = (conn) => {
+  return new Promise((resolve, reject) => {
+    conn.query(`SELECT * FROM member`, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
     });
+  });
 };
 
 const getDataEdit = (conn, username) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`SELECT * FROM member WHERE username = '${username}'`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `SELECT * FROM member WHERE username = '${username}'`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
 const checkDataMember = (conn, username) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`SELECT username FROM member WHERE username = '${username}'`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `SELECT username FROM member WHERE username = '${username}'`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
 const updateDataMember = (conn, data, nama, username, no_hp, alamat) => {
-    return new Promise((resolve, reject) => {
-        conn.query(`UPDATE member SET nama = '${nama}', username = '${username}', no_hp = '${no_hp}', alamat = '${alamat}' WHERE username = '${data}'`, (err, result) => {
-            if(err){
-                reject(err);
-            } else{
-                resolve(result);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `UPDATE member SET nama = '${nama}', username = '${username}', no_hp = '${no_hp}', alamat = '${alamat}' WHERE username = '${data}'`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
 
 const isAuthMember = (req, res, next) => {
-    if (req.session.isAuthMember){
-        req.session.isAuthAdmin = false;
-        next()
-    } else{
-        res.redirect('/')
-    }
-}
+  if (req.session.isAuthMember) {
+    req.session.isAuthAdmin = false;
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
 
 const isAuthAdmin = (req, res, next) => {
-    if (req.session.isAuthAdmin){
-        req.session.isAuthMember = false;
-        next()
-    } else{
-        res.redirect('/')
-    }
-}
-
-app.get('/', async (req, res) => {
-    let data = "";
-    req.session.isAuthAdmin = false;
+  if (req.session.isAuthAdmin) {
     req.session.isAuthMember = false;
-    res.render('login', {data})
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
+
+app.get("/", async (req, res) => {
+  let data = "";
+  req.session.isAuthAdmin = false;
+  req.session.isAuthMember = false;
+  res.render("login", { data });
 });
 
-app.get('/login', async (req, res) => {
-    let data = "";
-    req.session.isAuthAdmin = false;
-    req.session.isAuthMember = false;
-    res.render('login', {data})
+app.get("/login", async (req, res) => {
+  let data = "";
+  req.session.isAuthAdmin = false;
+  req.session.isAuthMember = false;
+  res.render("login", { data });
 });
 
-app.get('/daftar', async (req, res) => {
-    let isExist = ""
-    res.render('daftar', {isExist})
+app.get("/daftar", async (req, res) => {
+  let isExist = "";
+  res.render("daftar", { isExist });
 });
 
-app.get('/homeAdmin', isAuthAdmin, async (req, res) => {
-    let dataSession = req.session.data;
-    console.log("Auth Admin : " + req.session.isAuthAdmin)
-    res.render('homeAdmin', {dataSession})
+app.get("/homeAdmin", isAuthAdmin, async (req, res) => {
+  let dataSession = req.session.data;
+  console.log("Auth Admin : " + req.session.isAuthAdmin);
+  res.render("homeAdmin", { dataSession });
 });
 
-app.get('/homeMember', isAuthMember, async (req, res) => {
-    let dataSession = req.session.data
-    console.log("Auth Member : " + req.session.isAuthMember)
-    res.render('homeMember', {dataSession})
+app.get("/homeMember", isAuthMember, async (req, res) => {
+  let dataSession = req.session.data;
+  console.log("Auth Member : " + req.session.isAuthMember);
+  res.render("homeMember", { dataSession });
 });
 
-app.get('/dataMember', isAuthAdmin, async (req, res) => {
-    const conn = await dbConnect();
-    let dataSession = req.session.data;
-    let dataMember = await getDataMember(conn);
-    res.render('dataMember', {dataSession, dataMember})
+app.get("/dataMember", isAuthAdmin, async (req, res) => {
+  const conn = await dbConnect();
+  let dataSession = req.session.data;
+  let dataMember = await getDataMember(conn);
+  res.render("dataMember", { dataSession, dataMember });
 });
 
-app.get('/edit/:data', async (req, res) => {
-    const conn = await dbConnect();
-    let dataSession = req.session.data
-    let dataMember = await getDataMember(conn);
-    res.render('dataMember', {dataSession, dataMember})
-
+app.get("/edit/:data", async (req, res) => {
+  const conn = await dbConnect();
+  let dataSession = req.session.data;
+  let dataMember = await getDataMember(conn);
+  res.render("dataMember", { dataSession, dataMember });
 });
 
-app.get('/logout', async (req, res) => {
-    let data = "";
-    req.session.isAuthAdmin = false;
-    req.session.isAuthMember = false;
-    res.redirect('login')
+app.get("/logout", async (req, res) => {
+  let data = "";
+  req.session.isAuthAdmin = false;
+  req.session.isAuthMember = false;
+  res.redirect("login");
+});
+
+app.get("/cabang", isAuthMember, async (req, res) => {
+  const conn = await dbConnect();
+  let dataSession = req.session.data;
+  res.render("cabang", { dataSession });
+});
+
+app.get("/reservasi", isAuthMember, async (req, res) => {
+  const conn = await dbConnect();
+  let dataSession = req.session.data;
+  res.render("reservasi", { dataSession });
+});
+app.get("/historiReservasi", isAuthMember, async (req, res) => {
+  const conn = await dbConnect();
+  let dataSession = req.session.data;
+  res.render("historiReservasi", { dataSession });
 });
 
 //POST METHOD
 
-app.post('/login', async (req, res) => {
-    const conn = await dbConnect();
-    const { username, password } = req.body;
-    const hashed_pass = crypto.createHash('sha256').update(password).digest('base64');
-    let data = "";
-    console.log(hashed_pass);
-    if (username.length > 0 && password.length > 0){
-        const dataAdmin = await getCheckAdmin(conn, username, hashed_pass);
-        const dataMember = await getCheckMember(conn, username, hashed_pass);
+app.post("/login", async (req, res) => {
+  const conn = await dbConnect();
+  const { username, password } = req.body;
+  const hashed_pass = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("base64");
+  let data = "";
+  console.log(hashed_pass);
+  if (username.length > 0 && password.length > 0) {
+    const dataAdmin = await getCheckAdmin(conn, username, hashed_pass);
+    const dataMember = await getCheckMember(conn, username, hashed_pass);
 
-        if (dataAdmin.length > 0){
-            req.session.data = dataAdmin[0].nama;
-            req.session.isAuthAdmin = true;
-            // console.log(req.session.data);
-            res.redirect('/homeAdmin');
-        } else if (dataMember.length > 0){
-            req.session.data = dataMember[0].nama;
-            req.session.isAuthMember = true;
-            // console.log(req.session.data);
-            res.redirect('/homeMember');
-        } else{
-            data = "Username atau Password salah!";
-            res.render('login', {data});
-        }
-        
+    if (dataAdmin.length > 0) {
+      req.session.data = dataAdmin[0].nama;
+      req.session.isAuthAdmin = true;
+      // console.log(req.session.data);
+      res.redirect("/homeAdmin");
+    } else if (dataMember.length > 0) {
+      req.session.data = dataMember[0].nama;
+      req.session.isAuthMember = true;
+      // console.log(req.session.data);
+      res.redirect("/homeMember");
+    } else {
+      data = "Username atau Password salah!";
+      res.render("login", { data });
     }
-    conn.release();
+  }
+  conn.release();
 });
 
-app.post('/daftar', async (req, res) => {
-    const conn = await dbConnect();
-    const { nama, username, password, retypepassword, nomorhp, alamat } = req.body;
-    const hashed_pass = crypto.createHash('sha256').update(password).digest('base64');
-    let checkDataExist = await checkDataMember(conn, username);
-    let data = "";
-    let isExist = "";
-    let status = true;
-    // console.log(hashed_pass);
-    if (password == retypepassword){
-        status = true;
-    } else{
-        status = false;
-    }
+app.post("/daftar", async (req, res) => {
+  const conn = await dbConnect();
+  const { nama, username, password, retypepassword, nomorhp, alamat } =
+    req.body;
+  const hashed_pass = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("base64");
+  let checkDataExist = await checkDataMember(conn, username);
+  let data = "";
+  let isExist = "";
+  let status = true;
+  // console.log(hashed_pass);
+  if (password == retypepassword) {
+    status = true;
+  } else {
+    status = false;
+  }
 
-    if (nama != undefined && username != undefined && status == true && nomorhp != undefined && alamat != undefined){
-        console.log(checkDataExist.length)
-        if (checkDataExist.length == 0){
-            console.log("Ter INSERT data nya")
-            const insert = await postDaftar(conn, nama, username, hashed_pass, nomorhp, alamat);
-            res.render('login', {data})
-        } else{
-            isExist = "Username sudah digunakan, silakan ganti!"
-            res.render('daftar', {isExist})
-        }
-    } else{
-        isExist = "Data yang Anda masukkan tidak valid!"
-        res.render('daftar', {isExist})
+  if (
+    nama != undefined &&
+    username != undefined &&
+    status == true &&
+    nomorhp != undefined &&
+    alamat != undefined
+  ) {
+    console.log(checkDataExist.length);
+    if (checkDataExist.length == 0) {
+      console.log("Ter INSERT data nya");
+      const insert = await postDaftar(
+        conn,
+        nama,
+        username,
+        hashed_pass,
+        nomorhp,
+        alamat
+      );
+      res.render("login", { data });
+    } else {
+      isExist = "Username sudah digunakan, silakan ganti!";
+      res.render("daftar", { isExist });
     }
-    
-    conn.release();
+  } else {
+    isExist = "Data yang Anda masukkan tidak valid!";
+    res.render("daftar", { isExist });
+  }
+
+  conn.release();
 });
 
-app.post('/edit/:data', async (req, res) => {
-    const conn = await dbConnect();
-    const {data} = req.params
-    const {username, nama, nomorhp, alamat} = req.body
-    // console.log(data)
-    // console.log(nama)
-    // console.log(username)
-    // console.log(nomorhp)
-    // console.log(alamat)
-    // console.log(req.session.data)
+app.post("/edit/:data", async (req, res) => {
+  const conn = await dbConnect();
+  const { data } = req.params;
+  const { username, nama, nomorhp, alamat } = req.body;
+  // console.log(data)
+  // console.log(nama)
+  // console.log(username)
+  // console.log(nomorhp)
+  // console.log(alamat)
+  // console.log(req.session.data)
 
-    let dataSession = req.session.data
-    let dataMember = await getDataMember(conn);
-    const dataEdit = await getDataEdit(conn, data)
-    const updateData = await updateDataMember(conn, data, nama, username, nomorhp, alamat)
+  let dataSession = req.session.data;
+  let dataMember = await getDataMember(conn);
+  const dataEdit = await getDataEdit(conn, data);
+  const updateData = await updateDataMember(
+    conn,
+    data,
+    nama,
+    username,
+    nomorhp,
+    alamat
+  );
 
-    conn.release();
-    res.redirect('/dataMember')
+  conn.release();
+  res.redirect("/dataMember");
 });
 
 app.listen(PORT, () => {
-    console.log("Ready!")
+  console.log("Ready!");
 });
