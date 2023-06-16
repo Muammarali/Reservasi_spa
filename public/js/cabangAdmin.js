8edc36bc19e69889262e708e6647c3e97cbcdc8c
Wildan Rizki Nurfauzi <63538803+wildanrizkii@users.noreply.github.com>
2023-06-16 19:21:13 +0700

    test

--- public/js/cabangAdmin.js
+++ public/js/cabangAdmin.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".btnEdit");
 const btnTambah = document.querySelector(".btn-tambah");
 const btn = document.querySelectorAll(".close");
+const dropdownKota = document.getElementById('dropdownKota');
 
 function openModal(target) {
     document.getElementById(target).style.display = "flex";
@@ -10,7 +11,28 @@ function closeModal(target) {
     document.getElementById(target).style.display = "none";
 }
 
+function filterTableByKota() {
+    const selectedKota = dropdownKota.value;
+    const rows = document.querySelectorAll('table tbody tr');
+    
+    // console.log("Nilai selectedKota:", selectedKota);
+    
+    rows.forEach(row => {
+        const kotaCell = row.querySelector('td:nth-child(4)');
+        const kota = kotaCell.textContent.trim();
+
+        // console.log("Nilai kota:", kota);
+
+        if (selectedKota === "Semua" || kota === selectedKota) {
+            row.style.display = 'table-row';
+        } else {
+            row.style.display = 'none';
+        }
+    });
+}
+
 (function() {
+    dropdownKota.addEventListener('change', filterTableByKota);
 
     buttons.forEach(function(button) {
         button.addEventListener("click", function() {
@@ -34,26 +56,3 @@ function closeModal(target) {
     });
 
 })();
-
-const dropdownKota = document.getElementById('dropdownKota');
-dropdownKota.addEventListener('change', filterTableByKota);
-
-function filterTableByKota() {
-    const selectedKota = dropdownKota.value;
-    const rows = document.querySelectorAll('table tbody tr');
-    
-    // console.log("Nilai selectedKota:", selectedKota);
-    
-    rows.forEach(row => {
-        const kotaCell = row.querySelector('td:nth-child(4)');
-        const kota = kotaCell.textContent.trim();
-
-        // console.log("Nilai kota:", kota);
-
-        if (selectedKota === "Semua" || kota === selectedKota) {
-            row.style.display = 'table-row';
-        } else {
-            row.style.display = 'none';
-        }
-    });
-}
