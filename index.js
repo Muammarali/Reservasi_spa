61f3ce661eb8fa1729fd044f39d31f6e8659bc82
Muammarali <gastaa01@gmail.com>
2023-06-17 14:47:44 +0700

    upload gambar

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const pool = mysql.createPool({
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'public/images/');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpg');
   }
 });
 const upload = multer({ storage: storage })
